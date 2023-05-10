318289df33b5aee4d398f4834c02f4aa13d8881c
pangolin <2751999306@qq.com>
2023-05-10 23:17:37 +0800

    交互式绘制三分量图像

--- no_dangji_data_show.m
+++ no_dangji_data_show.m
@@ -1,60 +1,78 @@
-earthquakepath = 'C:\Users\wty\Downloads\百度网盘下载\SUMSUNG不宕机数据_SAC\SAC_vel\20230316085600.seed\LANT';
-data = dir(earthquakepath);
-filename1 = data(3).name;
-parts1 = strsplit(filename1,'.');
-direction1 = parts1{10}(3);
-filename2 = data(4).name;
-parts2 = strsplit(filename2,'.');
-direction2 = parts2{10}(3);
-filename3 = data(5).name;
-parts3 = strsplit(filename3,'.');
-direction3 = parts3{10}(3);
-
-[~,record1,~] = fget_sac(fullfile(earthquakepath,filename1));
-[~,record2,~] = fget_sac(fullfile(earthquakepath,filename2));
-[~,record3,~] = fget_sac(fullfile(earthquakepath,filename3));
-
-figure
-set(gca,'FontSize',20)
-subplot(3,1,1)
-for i =1:3
-    if eval(strcat('direction',num2str(i))) == 'E'
-        t = [1:length(eval(strcat('record',num2str(i))))];
-        t = t./100;
-        plot(t,eval(strcat('record',num2str(i))))
+%% 自动化绘制三分向数据波形
+do = 1;
+while(do == 1)
+    earthquakepath = input('请输入文件路径(须带引号)：');
+    
+    type = input('请选择数据类型("速度"/"位移"/"加速度")(须带引号)') ;
+    
+    if strcmp(type,'速度')
+        type = '速度(nm/s)';
+    elseif strcmp(type,'位移')
+        type = '位移(nm)';
+    elseif strcmp(type,'加速度')
+        type = '加速度(nm/s^2)';
     end
-end
-
-xlabel('时间(s)')
-ylabel('速度(nm/s)')
-title('BHE')
-
-subplot(3,1,2)
-for i =1:3
-    if eval(strcat('direction',num2str(i))) == 'N'
-        t = [1:length(eval(strcat('record',num2str(i))))];
-        t = t./100;
-        plot(t,eval(strcat('record',num2str(i))))
+    data = dir(earthquakepath);
+    filename1 = data(3).name;
+    parts1 = strsplit(filename1,'.');
+    direction1 = parts1{10}(3);
+    filename2 = data(4).name;
+    parts2 = strsplit(filename2,'.');
+    direction2 = parts2{10}(3);
+    filename3 = data(5).name;
+    parts3 = strsplit(filename3,'.');
+    direction3 = parts3{10}(3);
+    
+    [~,record1,~] = fget_sac(fullfile(earthquakepath,filename1));
+    [~,record2,~] = fget_sac(fullfile(earthquakepath,filename2));
+    [~,record3,~] = fget_sac(fullfile(earthquakepath,filename3));
+    
+    figure
+    set(gca,'FontSize',20)
+    subplot(3,1,1)
+    for i =1:3
+        if eval(strcat('direction',num2str(i))) == 'E'
+            t = 1:length(eval(strcat('record',num2str(i))));
+            t = t./100;
+            plot(t,eval(strcat('record',num2str(i))))
+            xlim([0 t(length(eval(strcat('record',num2str(i)))))])
+        end
     end
-end
-
-xlabel('时间(s)')
-ylabel('速度(nm/s)')
-title('BHN')
-
-subplot(3,1,3)
-for i =1:3
-    if eval(strcat('direction',num2str(i))) == 'Z'
-        t = [1:length(eval(strcat('record',num2str(i))))];
-        t = t./100;
-        plot(t,eval(strcat('record',num2str(i))))
+    
+    xlabel('时间(s)')
+    ylabel(type)
+    title('BHE')
+    
+    subplot(3,1,2)
+    for i =1:3
+        if eval(strcat('direction',num2str(i))) == 'N'
+            t = 1:length(eval(strcat('record',num2str(i))));
+            t = t./100;
+            plot(t,eval(strcat('record',num2str(i))))
+            xlim([0 t(length(eval(strcat('record',num2str(i)))))])
+        end
     end
+    
+    xlabel('时间(s)')
+    ylabel(type)
+    title('BHN')
+    
+    subplot(3,1,3)
+    for i =1:3
+        if eval(strcat('direction',num2str(i))) == 'Z'
+            t = 1:length(eval(strcat('record',num2str(i))));
+            t = t./100;
+            plot(t,eval(strcat('record',num2str(i))))
+            xlim([0 t(length(eval(strcat('record',num2str(i)))))])
+        end
+    end
+    
+    xlabel('时间(s)')
+    ylabel(type)
+    title('BHZ')
+
+    input('是否继续？(1/0)')
 end
-
-xlabel('时间(s)')
-ylabel('速度(nm/s)')
-title('BHZ')
-
 
 
 %%
