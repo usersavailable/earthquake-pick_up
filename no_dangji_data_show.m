985cb7ae81b321453c23f5f1535c480d87532a31
pangolin <2751999306@qq.com>
2023-05-12 21:51:35 +0800

    懒人all event 1 figure

--- no_dangji_data_show.m
+++ no_dangji_data_show.m
@@ -1,4 +1,4 @@
-%% 自动化绘制三分向数据波形
+%% 自动化(交互型)绘制三分向数据波形
 do = 1;
 while(do == 1)
     earthquakepath = input('请输入文件路径(须带引号)：');
@@ -35,7 +35,7 @@ while(do == 1)
 
 
 
-    figure
+
     set(gca,'FontSize',20)
     subplot(3,1,1)
     for i =1:3
@@ -46,12 +46,14 @@ while(do == 1)
             xlim([0 t(length(eval(strcat('record',num2str(i)))))])
         end
     end
-    
+    hold on
+
     xlabel('时间(s)')
     ylabel(type)
     title('BHE')
     
     subplot(3,1,2)
+    set(gca,'FontSize',20)
     for i =1:3
         if eval(strcat('direction',num2str(i))) == 'N'
             t = 1:length(eval(strcat('record',num2str(i))));
@@ -60,12 +62,14 @@ while(do == 1)
             xlim([0 t(length(eval(strcat('record',num2str(i)))))])
         end
     end
-    
+    hold on
+
     xlabel('时间(s)')
     ylabel(type)
     title('BHN')
     
     subplot(3,1,3)
+    set(gca,'FontSize',20)
     for i =1:3
         if eval(strcat('direction',num2str(i))) == 'Z'
             t = 1:length(eval(strcat('record',num2str(i))));
@@ -74,13 +78,113 @@ while(do == 1)
             xlim([0 t(length(eval(strcat('record',num2str(i)))))])
         end
     end
-    
+    hold on
+
     xlabel('时间(s)')
     ylabel(type)
     title('BHZ')
 
     do = input('是否继续？(1/0)');
 end
+
+
+%%
+%% 自动化(懒人式)绘制所有地震时间同一台站的三分向数据波形
+path0 = input('请输入文件路径(须带引号)：');
+station = input("请输入选用的台站");
+Fs = 100;
+Fs1 = 50;
+type = input('请选择数据类型("速度"/"位移"/"加速度")(须带引号)') ;
+path1 = dir(path0);
+
+if strcmp(type,'速度')
+    type = '速度(nm/s)';
+elseif strcmp(type,'位移')
+    type = '位移(nm)';
+elseif strcmp(type,'加速度')
+    type = '加速度(nm/s^2)';
+end
+
+for i = 1:length(path1)-2
+    path2 = dir(fullfile(path0,path1(i+2).name));
+    for j = 1:4
+        if (path2(j+2).name == station)
+
+        data = dir(fullfile(path0,path1(i+2).name,path2(j+2).name));
+        filename1 = data(3).name;
+        parts1 = strsplit(filename1,'.');
+        direction1 = parts1{10}(3);
+        filename2 = data(4).name;
+        parts2 = strsplit(filename2,'.');
+        direction2 = parts2{10}(3);
+        filename3 = data(5).name;
+        parts3 = strsplit(filename3,'.');
+        direction3 = parts3{10}(3);
+        
+        [~,record1,h1] = fget_sac(fullfile(path0,path1(i+2).name,path2(j+2).name,filename1));
+        [~,record2,h2] = fget_sac(fullfile(path0,path1(i+2).name,path2(j+2).name,filename2));
+        [~,record3,h3] = fget_sac(fullfile(path0,path1(i+2).name,path2(j+2).name,filename3));
+        
+    
+        record1 = resample(record1,Fs1,Fs);  %对数据进行重采样，采样后数据长度为原来的1/5
+        record2 = resample(record2,Fs1,Fs);
+        record3 = resample(record3,Fs1,Fs);
+    
+    
+    
+    
+        % set(gca,'FontSize',20)
+        subplot(3,1,1)
+        for k =1:3
+            if eval(strcat('direction',num2str(k))) == 'E'
+                t = 1:length(eval(strcat('record',num2str(k))));
+                t = t./Fs1;
+                plot(t,eval(strcat('record',num2str(k))))
+                xlim([0 t(length(eval(strcat('record',num2str(k)))))])
+            end
+        end
+        hold on
+    
+        xlabel('时间(s)')
+        ylabel(type)
+        title('BHE')
+        
+        subplot(3,1,2)
+        % set(gca,'FontSize',20)
+        for k =1:3
+            if eval(strcat('direction',num2str(k))) == 'N'
+                t = 1:length(eval(strcat('record',num2str(k))));
+                t = t./Fs1;
+                plot(t,eval(strcat('record',num2str(k))))
+                xlim([0 t(length(eval(strcat('record',num2str(k)))))])
+            end
+        end
+        hold on
+    
+        xlabel('时间(s)')
+        ylabel(type)
+        title('BHN')
+        
+        subplot(3,1,3)
+        % set(gca,'FontSize',20)
+        for k =1:3
+            if eval(strcat('direction',num2str(k))) == 'Z'
+                t = 1:length(eval(strcat('record',num2str(k))));
+                t = t./Fs1;
+                plot(t,eval(strcat('record',num2str(k))))
+                xlim([0 t(length(eval(strcat('record',num2str(k)))))])
+            end
+        end
+        hold on
+    
+        xlabel('时间(s)')
+        ylabel(type)
+        title('BHZ')
+
+        end
+    end
+end
+
 
 
 %%
